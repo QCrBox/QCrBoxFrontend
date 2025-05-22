5f3c605ec64a2439fa767527557a4728a3db7b1a
Jamie Court <jc2a23@soton.ac.uk>
2025-05-22 16:32:01 +0100

    Codify process_step object to store process metadata

--- qcrbox_frontend/qcrbox/models.py
+++ qcrbox_frontend/qcrbox/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+
+class process_step(models.Model):
+    application = models.CharField(max_length=255,)
+    infile_uuid = models.CharField(max_length=255,)
+    outfile_uuid = models.CharField(max_length=255, blank=True, null=True,)
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL,)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL,)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE,)
