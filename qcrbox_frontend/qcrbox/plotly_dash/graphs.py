c988bf2bbcb17d8237a716089f02cc61b4f3bf51
Jamie Court <jc2a23@soton.ac.uk>
2025-07-02 16:21:23 +0100

    Fix issue where dcc plots would not render due to missing statics

--- qcrbox_frontend/qcrbox/plotly_dash/graphs.py
+++ qcrbox_frontend/qcrbox/plotly_dash/graphs.py
@@ -1,5 +1,27 @@
-# import plotly.express as px
+'''Plotly Graphs
 
-# from dash import dcc
+A module to store methods for creating plots to be included as part of a
+Plotly Dash app in the QCrBox Frontend
 
-# Produce plots for the dashboard
+'''
+
+import plotly.express as px
+
+from dash import dcc
+
+def tree_plot():
+    '''The main Tree Plot for the Tree Dashboard'''
+
+    ##### PLACEHOLDER #####
+
+    fig = px.bar(x=['1', '2', '3'], y=[1, 2, 3])
+
+    graph_component = dcc.Graph(
+        figure=fig,
+        style={
+            'width': '100%',
+            'height': '100%;',
+        }
+    )
+
+    return graph_component
--- qcrbox_frontend/qcrbox/plotly_dash/plotly_app.py
+++ qcrbox_frontend/qcrbox/plotly_dash/plotly_app.py
@@ -1,37 +1,19 @@
-import dash
-from dash import dcc, html
+'''Plotly App Config
+
+Plotly App configuration and layouts
+for QCrBox Frontend
+
+'''
+
+from dash import html
 
 from django_plotly_dash import DjangoDash
 
-app = DjangoDash('SimpleExample')   # replaces dash.Dash
+from . import graphs
+
+app = DjangoDash('SimpleExample')
 
 app.layout = html.Div([
-    dcc.RadioItems(
-        id='dropdown-color',
-        options=[{'label': c, 'value': c.lower()} for c in ['Red', 'Green', 'Blue']],
-        value='red'
-    ),
-    html.Div(id='output-color'),
-    dcc.RadioItems(
-        id='dropdown-size',
-        options=[{'label': i,
-                  'value': j} for i, j in [('L','large'), ('M','medium'), ('S','small')]],
-        value='medium'
-    ),
-    html.Div(id='output-size')
+    graphs.tree_plot()
 
 ])
-
-@app.callback(
-    dash.dependencies.Output('output-color', 'children'),
-    [dash.dependencies.Input('dropdown-color', 'value')])
-def callback_color(dropdown_value):
-    return "The selected color is %s." % dropdown_value
-
-@app.callback(
-    dash.dependencies.Output('output-size', 'children'),
-    [dash.dependencies.Input('dropdown-color', 'value'),
-     dash.dependencies.Input('dropdown-size', 'value')])
-def callback_size(dropdown_color, dropdown_size):
-    return "The chosen T-shirt is a %s %s one." %(dropdown_size,
-                                                  dropdown_color)
